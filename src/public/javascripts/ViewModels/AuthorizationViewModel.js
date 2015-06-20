91a0ebde7cc14de36ab800cbced2bcde15a71e41
dectony <tony.zasenko@gmail.com>
2015-06-20 19:29:19 +0300

    SignIn/SignUp added

--- src/public/javascripts/ViewModels/AuthorizationViewModel.js
+++ src/public/javascripts/ViewModels/AuthorizationViewModel.js
@@ -4,10 +4,38 @@
     function AuthorizationViewModel() {
         var self = {};
 
+        self.userName = ko.observable();
+        self.password = ko.observable();
+
+        self.lastName = ko.observable();
+        self.firstName = ko.observable();
+
+        self.confirmPassword = ko.observable();
+
         self.isAuthorizationViewModelEnabled = ko.observable(false);
 
         self.ShowForm = function() {
             self.isAuthorizationViewModelEnabled(!self.isAuthorizationViewModelEnabled());
+        };
+
+        self.logIn = function () {
+            var data = {
+                "username": self.userName(),
+                "password": self.password()
+            };
+            $.post("/api/user/login", data, function(returnedData) {
+                var test = returnedData;
+            });
+        };
+
+        self.register = function () {
+            var data = {
+                "username": self.userName(),
+                "password": self.password()
+            };
+            $.post("/api/user/register", data, function (returnedData) {
+                var test = returnedData;
+            });
         };
 
         return self;
